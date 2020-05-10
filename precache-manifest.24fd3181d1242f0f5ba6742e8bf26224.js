4550466af050982e3ae37c1d3c0a39c773450039
ermias kidane <hadeklte21@gmail.com>
2020-05-10 15:40:06 +0100

    Updates

---
+++ precache-manifest.24fd3181d1242f0f5ba6742e8bf26224.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef88a672287c8765ac16d2bf021a72e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a2b5c54510358631b897",
+    "url": "/portfolio/static/css/main.4a1c5150.chunk.css"
+  },
+  {
+    "revision": "9ec58a0f3c194ebe9a51",
+    "url": "/portfolio/static/js/2.696e3cff.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/portfolio/static/js/2.696e3cff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2b5c54510358631b897",
+    "url": "/portfolio/static/js/main.1b50d973.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  },
+  {
+    "revision": "14b3b39ac78df0a940da2bfa44364564",
+    "url": "/portfolio/static/media/coffee-2.14b3b39a.jpg"
+  },
+  {
+    "revision": "cdcb7068e2c8cee52624a29a3f53c0fc",
+    "url": "/portfolio/static/media/eru-2 (Custom).cdcb7068.jpg"
+  },
+  {
+    "revision": "fe2cf5e2b2a432893bafdc01e1bf0df2",
+    "url": "/portfolio/static/media/mega-food (Custom).fe2cf5e2.png"
+  },
+  {
+    "revision": "06116e974e06bafab03a8f471843ebaf",
+    "url": "/portfolio/static/media/sprite.06116e97.svg"
+  }
+]);
